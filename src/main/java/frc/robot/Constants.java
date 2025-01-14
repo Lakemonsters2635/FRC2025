7779a9dd5b1e61c83ad12d6e2aceb5cfefaf2333
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2025-01-13 20:27:45 -0800

    initial commit, not tested

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,176 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.util.Units;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  
+  // joystick channels
+  public static final int RIGHT_JOYSTICK_CHANNEL = 1;
+  public static final int LEFT_JOYSTICK_CHANNEL = 0;
+
+  public static final double INCHES_PER_METER = 39.37;
+
+  // If you want to change the definition of what is front left etc. 
+
+  // For 180 switch the constants of encoder offsets, motor and encoder ids for front left and back right, and front right and back left
+  // Add 180 to all the encoder offsets
+
+  // For rotating front of the robot clockwise 90 degrees for motor and encoder constants and encoder offsets
+  // make the front left to front right, front right to back right, back right to back left, back left to front left
+  // Subtract 90 to all encoder offsets
+  // Switch robot's lenght to width
+
+  // For rotating front of the robot counter-clockwise 90 degrees for motor and encoder constants and encoder offsets
+  // make the front left to back left, back left to back right, back right to front right, front right to front left
+  // Add 90 to all encoder offsets
+  // Switch robot's lenght to width
+
+  // ROBOT WIDTH AND LENGHT
+  public static final double DRIVETRAIN_WHEELBASE_WIDTH =  25.5 / Constants.INCHES_PER_METER;
+  public static final double DRIVETRAIN_WHEELBASE_LENGTH = 21.5 / Constants.INCHES_PER_METER; 
+
+  // SWERVE MODULE STATES
+  public static final int FRONT_LEFT_MODULE_STATE_INDEX = 0;
+  public static final int FRONT_RIGHT_MODULE_STATE_INDEX = 1;
+  public static final int BACK_LEFT_MODULE_STATE_INDEX = 2;
+  public static final int BACK_RIGHT_MODULE_STATE_INDEX = 3;
+
+  // FRONT LEFT
+  public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 7; //1
+  public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_ENCODER = 2; //1
+  public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 8; //2
+  public static final double FRONT_LEFT_ANGLE_OFFSET_COMPETITION = Math.toRadians(0); //3.0346
+
+  // FRONT RIGHT
+  public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 5; //7
+  public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_ENCODER = 3; //0
+  public static final int DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 6; //8
+  public static final double FRONT_RIGHT_ANGLE_OFFSET_COMPETITION = Math.toRadians(0); //2.9835
+
+  // BACK LEFT
+  public static final int DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 1; //3
+  public static final int DRIVETRAIN_BACK_LEFT_ANGLE_ENCODER = 0; //3
+  public static final int DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 2; //10
+  public static final double BACK_LEFT_ANGLE_OFFSET_COMPETITION = Math.toRadians(0); // 3.0775
+
+  // BACK RIGHT
+  public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 3; //5
+  public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_ENCODER = 1;//2
+  public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 4; //6
+  public static final double BACK_RIGHT_ANGLE_OFFSET_COMPETITION = Math.toRadians(0); //3.01
+
+  // hat constants 
+  public static final int HAT_JOYSTICK_TRIM_POSITION = RIGHT_JOYSTICK_CHANNEL;
+  public static final int HAT_JOYSTICK_TRIM_ROTATION = LEFT_JOYSTICK_CHANNEL;
+  public static final double HAT_POWER_MOVE = 0.1;
+  public static final double HAT_POWER_ROTATE = 0.3;
+  // Hat trim target speed is 15 degrees per second
+  // One time step is 0.02 seconds
+  // 0.3 degrees per time step is our target change when the hat is active
+  public static final double HAT_POSE_TARGET_PER_TIME_STEP = -0.3; // negative is raising the arm
+  
+  public static final int HAT_POV_MOVE_LEFT = 270;
+  public static final int HAT_POV_MOVE_RIGHT = 90;
+  public static final int HAT_POV_MOVE_FORWARD = 0;
+  public static final int HAT_POV_MOVE_BACK = 180;
+  public static final int HAT_POV_0 = 0; // Left hat up
+  public static final int HAT_POV_180 = 180; // Left hat down
+  public static final int HAT_POV_ROTATE_LEFT = 270;
+  public static final int HAT_POV_ROTATE_RIGHT = 90;
+  /*Changed max speed from 2pi rad/s since max no load speed is 15 ft/s and with our robot
+  the radius from the center of our robot to the center of the module is 16.5 inches therefore
+  with a max commanded angular speed of 2pi radians our max commanded speed is 8.6 ft/s which
+  is about half the theoretical maximum. We don't want to go to the absolute maximum which would
+  approximately be 4pi rad/s so we choose 3pi rad/s as the maximum.
+  Note: if we run into problems during autos with it lagging, we may want to revert it back to 2pi rad/s. */
+  public static final double kMaxModuleAngularSpeedRadiansPerSecond = 3 * Math.PI; 
+  //We set it to 9pi rad/s^2 so that we get to the maximum speed within 1/3 of a second
+  //We may want to back off on this if autos start lagging
+  public static final double kMaxModuleAngularAccelerationRadiansPerSecondSquared = 12 * Math.PI;
+
+  public static final double maxModuleLinearSpeed = 1.75;
+  public static final double maxModuleLinearAccelaration = 8;
+  // public static final double maxModuleLinearSpeed = 3.5;
+  // public static final double maxModuleLinearAccelaration = 16;
+
+  public static final int kEncoderCPR = 42; // neo encoder ticks per revolution
+  public static final double kWheelDiameterMeters = 4.0 / 39.37;
+  public static final double kCalibrationFactor = 0.9; // we tested the actual traveled distance vs the distance the robot thinks it traveled to come up with this factor
+  public static final double kDriveEncoderDistancePerPulse =
+      // Assumes the encoders are directly mounted on the wheel shafts
+      // (kWheelDiameterMeters * Math.PI) * (1.0 / (45.0 / 15.0) / (17.0 / 27.0) / (50.0 / 14.0));  //Mark 4i L2 Gear Ratio // should be 5.14 to 1 four L4 on BunnyBot
+      (kWheelDiameterMeters * Math.PI) * 5.14 * kCalibrationFactor;  //Mark 4i L2 Gear Ratio // should be 5.14 to 1 four L4 on BunnyBot
+      // 0.0014785364645989762;
+
+      // Some additional notes... 
+      //
+      //6.74603174603 gear ratio for eclipse...
+      // 42 counts per revolution of the motor
+      // 6.746 * 42 counts per revolution of the wheel
+      // 283.333333333 counts per revolution of the wheel
+      // (kWheelDiameterMeters * Math.PI) circumference in meters
+      // (kWheelDiameterMeters * Math.PI) / 283.3333  meters per count i.e. distance per pulse
+      // 0.0011265396 meters per pulse for eclipse
+      //
+      //5.14 L4 gear ratio for bunnybot
+      //42 counts per revolution of the motor
+      //5.14 * 42 counts per revolution of the wheel
+      //215.88 counts per revolution of the wheel
+      // (kWheelDiameterMeters * Math.PI) circumference in meters
+      // (kWheelDiameterMeters * Math.PI)/ 215.88 meters per count i.e. distance per pulse
+      // 0.0014785364645989762 meters per pulse for bunnybot2024
+
+  // put into manual mode, manually read position and rotate wheel
+
+  public static final double kTurningEncoderDistancePerPulse =
+      // Assumes the encoders are on a 1:1 reduction with the module shaft.
+      (2 * Math.PI) / (double) kEncoderCPR;
+
+  public static final double kPModuleTurningController = 0.5;
+
+  public static final double kPModuleDriveController = 0;
+
+  public static final int kDriverControllerPort = 0;
+  public static final int kOperatorControllerPort = 1;
+
+  // VISION CONSTANTS
+  public static double OBJECT_DETECTION_LATENCY = 0.217; // seconds
+  public static final double VISION_NOTE_CAM_TILT = Units.degreesToRadians(0);
+  public static final double VISION_APRIL_TAG_PRO_TILT = Units.degreesToRadians(0);
+
+  // {x, y} in inches, +y is robots front, +x is to the right
+  public static final double[] VISION_TOTE_CAM_OFFSET = {-Units.metersToInches((DRIVETRAIN_WHEELBASE_WIDTH/2)) + 4, -Units.metersToInches(DRIVETRAIN_WHEELBASE_LENGTH/2)}; // TODO: calculate these values based from the center of the robot
+  public static final double[] VISION_BALLOON_CAM_OFFSET = {};
+
+  public static final int TARGET_TRIGGER_DISTANCE_APRIL_TAG = 0;
+
+  // AUTO CONSTANTS
+  
+  // AUTOMOVESWERVE CONSTANTS
+  public static final double CHANGE_IN_X_PER_SECOND= 0.714;
+  public static final double CHANGE_IN_Y_PER_SECOND= 0.717;
+
+  // Conversions
+  public static final double FEET_TO_METERS = 3.281;
+
+  //BUTTON BINDINGS
+
+  // right buttons
+  public static final int SWERVE_RESET_BUTTON = 7;
+  public static final int ZERO_ODOMETRY_BUTTON = 11;
+
+  // left buttons
+
+}
