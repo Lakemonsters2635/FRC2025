05fcc00895bfe28aee51cb189c2a9dc536d5f46e
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2025-03-03 19:22:28 -0800

    WIP PID vision path

--- src/main/java/frc/robot/commands/VisionPureAutoCommand.java
+++ src/main/java/frc/robot/commands/VisionPureAutoCommand.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
 // import frc.robot.subsystems.ObjectTrackerSubsystem.Detection;
 import frc.robot.subsystems.Detection;
 import frc.robot.subsystems.DrivetrainSubsystem;
@@ -42,6 +43,7 @@ public class VisionPureAutoCommand extends Command {
   double m_x_target;
   double m_y_target;
   double m_rot_target;
+  double m_c_target; //hypotneuse
 
   double m_x_start;
   double m_y_start;
@@ -112,14 +114,15 @@ public class VisionPureAutoCommand extends Command {
     // TODO: need to set the field x, y, rotation target.
     // TODO the x,y,rot will be based on the initial state + what was defined in the constructor based on desired relative positoin from the april tag.
     Pose2d fieldDeltaPose = visionAutoData(m_xPrime, m_zPrime, m_finalYa, m_tagID);
-    double m_x_target = 0;
-    double m_y_target = 0;
-    double m_rot_target = 0;
+    m_x_target = fieldDeltaPose.getX();
+    m_y_target = fieldDeltaPose.getY();
+    m_rot_target = fieldDeltaPose.getRotation().getDegrees();
+    m_c_target = Math.sqrt(Math.pow(m_x_target, 2) + Math.pow(m_y_target, 2));
 
     // get the starting pose so we can calculate fade-in for speed
-    double m_x_start = 0;
-    double m_y_start = 0;
-    double m_rot_start = 0;
+    m_x_start = m_dts.getPose().getX();
+    m_y_start = m_dts.getPose().getY();
+    m_rot_start = m_dts.getPose().getRotation().getDegrees();
 
     // NOTE: actual pose of the robot will be updated in execute.
 
@@ -161,32 +164,41 @@ public class VisionPureAutoCommand extends Command {
     // Get the vision
 
     // TODO this pose will be current pose from odemetry.
-    double x_pose = 0;
-    double y_pose = 0;
-    double rot_pose = 0;
-
+    double x_pose = m_dts.getPose().getX();
+    double y_pose = m_dts.getPose().getY();
+    double rot_pose = m_dts.getPose().getRotation().getDegrees();
+    
     // TODO need something to fade-in the acceleration so we don't brown out the  robot.
     // we can do this by remembering how far we are from the start... maybe it is better to take a hack 
     // use m_{xy,yrot}_start for fade-in... need to figure out how far of a distance this needs to be faded in from.
     // this fade in distance likely needs to be an initialization parameter.
+    double distanceFromStart = Math.sqrt(Math.pow((x_pose - m_x_start), 2) + Math.pow((y_pose - m_y_start), 2)); //use pythagoream 
+    double fadeInDistance = 1.5; //in meters
+    double distance_clamp = PURE_VISION_MAX_M_PER_SEC * (distanceFromStart/fadeInDistance);
+    distance_clamp = MathUtil.clamp(distance_clamp,0, PURE_VISION_MAX_M_PER_SEC);
+    double x_clamp = distance_clamp * (m_x_target/m_c_target); //distance_clamp * cos(theta)
+    double y_clamp = distance_clamp * (m_y_target/m_c_target); //distance_clamp * sin(theta)
 
     m_fb_x = MathUtil.clamp(
         m_visionSwerveController_x.calculate(x_pose, m_x_target), 
-        -1 * PURE_VISION_MAX_M_PER_SEC, PURE_VISION_MAX_M_PER_SEC
+        -1 * x_clamp, x_clamp
     );
     m_fb_y = MathUtil.clamp(
         m_visionSwerveController_y.calculate(y_pose, m_y_target), 
-        -1 * PURE_VISION_MAX_M_PER_SEC, PURE_VISION_MAX_M_PER_SEC
+        -1 * y_clamp, y_clamp
     );
     m_fb_rot = MathUtil.clamp(
-        m_visionSwerveController_rot.calculate(rot_pose, m_rot_target), 
+        m_visionSwerveController_rot.calculate(Math.toRadians(rot_pose), Math.toRadians(m_rot_target)), 
         -1 * PURE_VISION_MAX_RAD_PER_SEC, PURE_VISION_MAX_RAD_PER_SEC
     );
 
     // CLAMP the values so they are not too fast
     // TODO look at it to see if we want field relative or robot centric.
     // .    our equeitons simplify if we do robot centric.
-    m_dts.drive(m_fb_x, m_fb_y, m_fb_rot, true);
+    double driveX_Fraction = m_fb_x / Constants.maxModuleLinearSpeed;
+    double driveY_Fraction = m_fb_y / Constants.maxModuleLinearSpeed;
+    double driveRot_Fraction = m_fb_rot / Constants.kMaxModuleAngularSpeedRadiansPerSecond;
+    m_dts.drive(driveX_Fraction, driveY_Fraction, driveRot_Fraction, true);
     // xPrime = 23.5;
     // zPrime = -16.5;
     // finalYa = 0;
@@ -203,9 +215,10 @@ public class VisionPureAutoCommand extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    // TODO
-
-    return true;
+    if(m_visionSwerveController_x.atSetpoint() && m_visionSwerveController_y.atSetpoint() && m_visionSwerveController_rot.atSetpoint()){
+      return true;
+    }
+    return false;
   }
   
   /*
@@ -288,78 +301,5 @@ public class VisionPureAutoCommand extends Command {
    */
   public Pose2d visionAutoData(double xPrime, double zPrime, double finalYa){
     return visionAutoData(xPrime, zPrime, finalYa, -1);
-  }
-
-  /*
-   * Uses the visionAutoData function to create a path for the robot to move to a certain position from the closest april tag
-   * @param xPrime the x distance from the april tag
-   * @param zPrime the z distance from the april tag
-   * @param finalYa the final yaw angle for the robot to face
-   */
-  public Command visionCreatePath(double xPrime, double zPrime, double finalYa){
-    // since it is known that xPrime must be non-zero, we should just add 0.000001 to prevent
-    // a division by zero.  This protects us from inadvertently providing an invalid answer and allows 
-    // us to specify 0 for xPrime when this function is called which is more intuitive than forcing the 
-    // user of this function to enter 0.0001 manually to avoid an error.
-
-    //when defining zPrime and xPrime zPrime is positive going behind the april tag and xPrime is positive right of the april tag
-    xPrime += 0.00000112358;
-
-    return visionCreatePath(xPrime, zPrime, finalYa, -1);
-  }
-
-
-  /*
-   * Uses the visionAutoData function to create a path for the robot the robot to move to a certain position from a specific april tag
-   * @param xPrime the x distance from the april tag
-   * @param zPrime the z distance from the april tag
-   * @param finalYa the final yaw angle for the robot to face
-   * @param tagID the id of the april tag
-   */
-  public Command visionCreatePath(double xPrime, double zPrime, double finalYa, int tagID){
-    // since it is known that xPrime must be non-zero, we should just add 0.000001 to prevent
-    // a division by zero.  This protects us from inadvertently providing an invalid answer and allows 
-    // us to specify 0 for xPrime when this function is called which is more intuitive than forcing the 
-    // user of this function to enter 0.0001 manually to avoid an error.
-
-    //when defining zPrime and xPrime zPrime is positive going behind the april tag and xPrime is positive right of the april tag
-    xPrime += 0.00000112358;
-    
-    fieldDeltaPose = visionAutoData(xPrime, zPrime, finalYa, tagID);
-
-    return new SequentialCommandGroup(
-      new InstantCommand(()->SmartDashboard.putNumber("dts.getPose() x before",m_dts.getPose().getX())),
-      new InstantCommand(()->SmartDashboard.putNumber("dts.getPose() y before",m_dts.getPose().getY())),
-      new InstantCommand(()->SmartDashboard.putNumber("dts.getPose() rotation before",m_dts.getPose().getRotation().getDegrees())),
-      new InstantCommand(()->m_dts.stashAngle()),
-      new InstantCommand(()->m_dts.setFollowJoystick(false)),
-      new InstantCommand(()->m_dts.resetAngle()),
-      new InstantCommand(()->m_dts.zeroOdometry()),
-      m_dts.createVisionPath(
-        new Pose2d(
-          0, //botPose.getX(), 
-          0, //botPose.getY(), 
-          new Rotation2d(heading)   // TODO need to explain this rotation offset and point to docs
-        ), 
-        new Translation2d(
-          (fieldDeltaPose.getX()/2), 
-          (fieldDeltaPose.getY()/2)
-        ), 
-        new Pose2d(
-          fieldDeltaPose.getX(),
-          fieldDeltaPose.getY(), 
-          new Rotation2d(heading)
-        ),
-        fieldDeltaPose.getRotation().getDegrees()
-        // finalAngle //heading+(Math.PI/2)
-        // ,true
-      ),
-      new InstantCommand(()->m_dts.stopMotors()),
-      new InstantCommand(()->m_dts.restoreAngle()),
-      new InstantCommand(()->m_dts.setFollowJoystick(true)),
-      new InstantCommand(()->SmartDashboard.putNumber("dts.getPose() x after",m_dts.getPose().getX())),
-      new InstantCommand(()->SmartDashboard.putNumber("dts.getPose() y after",m_dts.getPose().getY())),
-      new InstantCommand(()->SmartDashboard.putNumber("dts.getPose() rotation after",m_dts.getPose().getRotation().getDegrees()))
-    );
   }
 }
